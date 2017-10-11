fcbd34228104634790300efdf35b680050bef51a
Abel Cabral Arruda <jcabral@id.uff.br>
2017-10-11 00:40:48 -0300

    Nova Versão 1.0

---
+++ tatoo.js
@@ -0,0 +1,11 @@
+(function life() {
+    var name, age;
+    age = new Date().getFullYear() - 1991;
+
+    if (age > 26) {
+        name = "Juracer";
+    } else {
+        name = "Abel";
+    }
+    alert("I am " + name);
+})();
